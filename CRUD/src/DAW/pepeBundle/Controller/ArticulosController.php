5a3400ae393ee62c659decf21b087edf5679bf69
fenix15100 <francisco.camacho15@gmail.com>
2018-02-05 18:09:15 +0100

    CRUD CONTRA BD

---
+++ CRUD/src/DAW/pepeBundle/Controller/ArticulosController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DAW\pepeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArticulosController extends Controller
+{
+}
---
+++ CRUD/src/DAW/pepeBundle/Tests/Controller/ArticulosControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DAW\pepeBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ArticulosControllerTest extends WebTestCase
+{
+}
